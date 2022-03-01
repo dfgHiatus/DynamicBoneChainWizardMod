b55401958f0cd97cf7bc556fc7a89f4417f2758e
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2022-02-28 18:14:07 -0600

    update version

--- DynamicBoneWizardMod/Wizard.cs
+++ DynamicBoneWizardMod/Wizard.cs
@@ -13,7 +13,7 @@ namespace Wizard
     {
         public override string Name => "DynamicBoneChainWizardMod";
         public override string Author => "dfgHiatus";
-        public override string Version => "1.0.0";
+        public override string Version => "1.0.1";
         public override string Link => "https://github.com/dfgHiatus/DynamicBoneChainWizardMod";
 
         public override void OnEngineInit()
